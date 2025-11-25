7ca54d2776d9da3d9f25d362da69b45dcb194e83
Christos Koumparakis <chriko@dtu.dk>
2025-11-25 11:33:48 +0100

    now all data should be inside the scenario folder

--- EnerHub2X/src/config.py
+++ EnerHub2X/src/config.py
@@ -2,7 +2,7 @@
 from dataclasses import dataclass
 import os
 
-DEFAULT_DATA_FILE = os.path.join(os.getcwd(), "Data_MeOH_UC.xlsx")
+DEFAULT_DATA_FILE = os.path.join(os.getcwd(), "scenarios", "Data_MeOH_Sens.xlsx")
 
 @dataclass
 class ModelConfig:
