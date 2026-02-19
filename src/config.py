09eef8b91ee9648b8174e8aa7c6cb70ba265e6b5
kubachri <chriskoumparakis@gmail.com>
2026-02-19 10:20:36 +0100

    final updates for scenarios

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@
 from dataclasses import dataclass
 import os
 
-DEFAULT_DATA_FILE = os.path.join(os.getcwd(), "scenarios", "Data_H2.xlsx")
+DEFAULT_DATA_FILE = os.path.join(os.getcwd(), "scenarios", "Data_Baseline.xlsx")
 
 @dataclass
 class ModelConfig:
